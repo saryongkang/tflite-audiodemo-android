fe3bea60cbf6099862f6d108fb2e9f7a1f42a8e9
Saryong Kang <saryongkang@google.com>
2020-08-07 09:53:54 +0900

    rollback contract for permission due to library bug

--- app/src/main/java/com/google/euphonia/trainableswitches/audiodemo/MainActivity.kt
+++ app/src/main/java/com/google/euphonia/trainableswitches/audiodemo/MainActivity.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.euphonia.trainableswitches.audiodemo.Utils.AUDIO_DEMO_TAG
@@ -13,15 +12,16 @@ import com.google.euphonia.trainableswitches.audiodemo.Utils.AUDIO_DEMO_TAG
 class MainActivity : AppCompatActivity() {
     private lateinit var audioDemoView: AudioDemoView
 
-    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            isGranted: Boolean ->
-        if (isGranted) {
-            Log.i(AUDIO_DEMO_TAG, "Audio permission granted :)");
-            audioDemoView.startAudioRecord();
-        } else {
-            Log.e(AUDIO_DEMO_TAG, "Audio permission not granted :(");
-        }
-    }
+    // TODO: Uncomment this when androidx.activity library become stable
+//    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+//            isGranted: Boolean ->
+//        if (isGranted) {
+//            Log.i(AUDIO_DEMO_TAG, "Audio permission granted :)");
+//            audioDemoView.startAudioRecord();
+//        } else {
+//            Log.e(AUDIO_DEMO_TAG, "Audio permission not granted :(");
+//        }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,11 +39,34 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+    // TODO: Remove this when androidx.activity library become stable
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_RECORD_AUDIO) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i(AUDIO_DEMO_TAG, "Audio permission granted :)");
+                audioDemoView.startAudioRecord();
+            } else {
+                Log.e(AUDIO_DEMO_TAG, "Audio permission not granted :(");
+            }
+        }
+    }
+
+
     private fun requestMicrophonePermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
             audioDemoView.startAudioRecord();
         } else {
-            requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+            // TODO: Uncomment this when androidx.activity library become stable
+//            requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+            requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO), REQUEST_RECORD_AUDIO)
         }
+    }
+
+    companion object {
+        const val REQUEST_RECORD_AUDIO = 1337
     }
 }
